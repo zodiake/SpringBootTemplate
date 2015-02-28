061c8e12b59827fc3eb12e7c9b7b483c31ebcf2c
yagamai <zodiake.wang@ihotkeys.com>
2015-02-28 15:57:59 +0800

    add kaptcha feature

--- src/main/java/application/Application.java
+++ src/main/java/application/Application.java
@@ -1,16 +1,22 @@
 package application;
 
+import java.util.Properties;
+
 import javax.annotation.Resource;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
 
 @SpringBootApplication
 public class Application {
@@ -39,5 +45,16 @@ public class Application {
 				throws Exception {
 			auth.userDetailsService(service);
 		}
+	}
+
+	@Bean
+	public DefaultKaptcha captchaProducer() {
+		DefaultKaptcha kaptcha = new DefaultKaptcha();
+		Properties properties=new Properties();
+		properties.put("kaptcha.textproducer.char.string", "0123456789");
+		properties.put("kaptcha.textproducer.char.length", "4");
+		Config config=new Config(properties);
+		kaptcha.setConfig(config);
+		return kaptcha;
 	}
 }
---
+++ src/main/java/application/domain/SignupForm.java
@@ -0,0 +1,7 @@
+package application.domain;
+
+public class SignupForm {
+	private String name;
+	private String password;
+	private String confirmPassword;
+}
